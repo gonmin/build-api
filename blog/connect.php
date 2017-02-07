993d6378a6f723dd6c52f5262ff154a97882bde4
jAVASCRIPT <2457907221@qq.com>
2017-02-07 09:34:29 +0800

    xiugai

--- blog/connect.php
+++ blog/connect.php
@@ -1,7 +1,7 @@
 <?php
 	header("Content-type: text/html; charset=utf-8");
 	session_start();
-	$link = mysql_connect('localhost','root','gongming') or die('数据库');
+	$link = mysql_connect('localhost','root','gongming') or die('数据库连接失败');
 	$open = mysql_select_db('blog') or die('打开数据库失败');
 	mysql_query('set names utf8');
 ?>
